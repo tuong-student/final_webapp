115dc20324be28382cffed5d58242c58e1ce8c0f
Your Name <thanhtuongnguyen1@gmail.com>
2021-12-07 17:15:02 +0700

    change database

--- src/main/java/com/final_project/data/UserSQL.java
+++ src/main/java/com/final_project/data/UserSQL.java
@@ -19,11 +19,12 @@ public class UserSQL {
             try {
                 // Hibernate setting
                 Properties settings = new Properties();
-                settings.put(Environment.DRIVER, "com.microsoft.sqlserver.jdbc.SQLServerDriver");
+                settings.put(Environment.DIALECT, "org.hibernate.dialect.PostgreSQLDialect");
+                settings.put(Environment.DRIVER, "org.postgresql.Driver");
                 settings.put(Environment.URL,
-                        "jdbc:sqlserver://SQL5080.site4now.net;databaseName=db_a7c9ad_demoshop271");
-                settings.put(Environment.USER, "db_a7c9ad_demoshop271_admin");
-                settings.put(Environment.PASS, "demoshop271");
+                        "jdbc:postgresql://ec2-52-23-40-80.compute-1.amazonaws.com:5432/dlv6cb1c59jnv");
+                settings.put(Environment.USER, "xnxtuawcjufsfv");
+                settings.put(Environment.PASS, "a3f56ae64225b15a1f3ac8257cd72f01b7d4d788a12363b74934d944782c7d31");
                 settings.put(Environment.HBM2DDL_AUTO, "update");
 
                 settings.put(Environment.SHOW_SQL, "true");
